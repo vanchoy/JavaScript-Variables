4f9a9cfa115293870b8d9d490c024d84497af3c5
Ivan Stoyanov <super.ludiq@gmail.com>
2019-02-11 12:22:50 +0100

    Create js-vars.js

---
+++ js-vars.js
@@ -0,0 +1,33 @@
+var name = 'Ivan';
+console.log(name);
+
+var lastName = 'Stoyanov';
+console.log(lastName);
+
+var age = 23;
+console.log(age);
+
+var fullAge = true;
+console.log(fullAge);
+
+console.log(name + age); //Ivan23
+console.log(age + age); //sum of both 
+
+var job, isMarried;
+
+console.log(job);
+
+job = 'teacher';
+isMarried = false;
+
+console.log(name + ' is a ' + age + ' years old ' + job + '. Is he married? ' + isMarried + '.');
+
+age = "thirty six";
+job = 'driver';
+
+console.log(name + ' is a ' + age + ' years old ' + job + '. Is he married? ' + isMarried + '.');
+
+var lastName = prompt('What is the last name?');
+console.log(lastName);
+
+alert(name + ' is a ' + age + ' years old ' + job + '. Is he married? ' + isMarried + '.');
